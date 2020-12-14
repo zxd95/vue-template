9a6e40eed227d49d1a618377aa35834f111d0208
zhangxiaodong <774793337@qq.com>
2020-12-14 11:57:21 +0800

    Update maike.js

--- maike.js
+++ maike.js
@@ -1,2 +1,3 @@
 1.剑指java自研框架-决胜spring源码  https://www.52pojie.cn/forum.php?mod=viewthread&tid=1167494
 2.Python3入门人工智能 掌握机器学习+深度学习 提升实战能力  https://www.52pojie.cn/thread-1291685-1-1.html
+3.掌握Java项目面试武功心法，面试弯道超车  https://www.52pojie.cn/thread-1213428-1-1.html
